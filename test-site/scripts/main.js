ec75976a1bc6d05e0bf61066dc0b90dbb60dcc88
Keily Hernandez <Keily-Northeastern@Keilys-MacBook-Air-2.local>
2024-01-12 13:24:03 -0500

    firefox test site

---
+++ test-site/scripts/main.js
@@ -0,0 +1,35 @@
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-logo.jpeg") {
+    myImage.setAttribute("src", "images/firefox.jpeg");
+  } else {
+    myImage.setAttribute("src", "images/firefox-logo.jpeg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+
+  myButton.onclick = () => {
+    setUserName();
+  };
