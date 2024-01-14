1523bbe2093af4f95b1782bcd7a9976b740fb193
Keily Hernandez <Keily-Northeastern@Keilys-MacBook-Air-2.local>
2024-01-14 11:17:24 -0500

    hw1 pt2

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -3,15 +3,15 @@ const myImage = document.querySelector("img");
 
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-logo.jpeg") {
-    myImage.setAttribute("src", "images/firefox.jpeg");
+  if (mySrc === "images/data2.jpeg") {
+    myImage.setAttribute("src", "images/books.jpeg");
   } else {
-    myImage.setAttribute("src", "images/firefox-logo.jpeg");
+    myImage.setAttribute("src", "images/data2.jpeg");
   }
 };
 
 let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
+let myHeading = document.querySelector("h2");
 
 function setUserName() {
     const myName = prompt("Please enter your name.");
@@ -19,7 +19,7 @@ function setUserName() {
       setUserName();
     } else {
       localStorage.setItem("name", myName);
-      myHeading.textContent = `Mozilla is cool, ${myName}`;
+      myHeading.textContent = `Hi, ${myName}`;
     }
   }
 
@@ -27,7 +27,7 @@ function setUserName() {
     setUserName();
   } else {
     const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+    myHeading.textContent = `Hi, ${storedName}`;
   }
 
   myButton.onclick = () => {
